af8d182d54078a35eae05fd761da874647955f6a
Robb Tvorik <ieatkimchi@gmail.com>
2014-10-01 22:47:53 -0400

    working on listview app

---
+++ app/src/main/java/app/com/example/hong/namethatofficial/MainActivity.java
@@ -0,0 +1,62 @@
+package app.com.example.hong.namethatofficial;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.AdapterView;
+import android.widget.Toast;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String[] electedPositions = {"Governor", "Lieutenant Governor", "Secretary of State", "Attorney General"};
+        final String[] electedOfficials = {"Rick Snyder -R", "Brian Calley -R", "Ruth Johnson -R", "Bill Schuette -R"};
+
+        ListAdapter positionListAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
+                electedPositions);
+
+        ListView positionView = (ListView) findViewById(R.id.mainView);
+
+        positionView.setAdapter(positionListAdapter);
+
+        positionView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+
+                String positionSelected = String.valueOf(electedOfficials[position]);
+                Toast.makeText(MainActivity.this, positionSelected, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
